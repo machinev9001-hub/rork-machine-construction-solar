4dbd436624f52925d12706a7b5f086970ad853c6
Paul J Slabbert <machinev9.001@gmail.com>
2025-12-17 16:59:58 +0200

    Enhance metro.config.js for CJS support and minification
    Added support for '.cjs' file extensions and updated transformer configuration to preserve class names and function names during minification.

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,20 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts = [...(config.resolver.sourceExts || []), 'cjs'];
+
+config.transformer = {
+...config.transformer,
+minifierConfig: {
+  keep_classnames: true,
+  keep_fnames: true,
+  mangle: {
+    keep_classnames: true,
+    keep_fnames: true,
+  },
+},
+};
+
+config.watchFolders = [__dirname];
+
 module.exports = config;
